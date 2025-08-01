313e170bd70981e8acaa916d6e15d8f49e5db489
raphaelrychard <raph.rych@gmail.com>
2025-08-01 09:32:40 -0400

    add: initial interface

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,33 +1,9 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import { useSelector, type TypedUseSelectorHook } from 'react-redux'
 
-
-/**
- * reducer -> as todas as informações que agente vai compartilhas entre os componentes da nossa aplicação.
- * slice -> pequenos pedaços de estados compartilhados
- * 
- * 
- */
-
-const todoSlice = createSlice({
-  name: 'todo',
-  initialState: ['Fazer café', 'Estudar Redux', 'Estudar Zustand'],
-
-  // vai adicionar quais ações a interface pode fazer no nosso estado
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo)
-    }
-  }
-})
-
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer
-  }
+  reducer: {}
 })
-
-export const { add } = todoSlice.actions
 
 export type RootState = ReturnType<typeof store.getState>
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
